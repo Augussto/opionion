5a78dcf91a2e350762db2d7a7b0c35ccc66406cb
Augussto <augusstocs@gmail.com>
2021-04-13 18:54:30 -0300

    v0.4

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 document.getElementById("btn-register").onclick = () => {
 	document.getElementById("login-container").style.display = "none";
 	document.getElementById("register-container").style.display = "flex";
+	document.getElementById("btn-login").style.display = "block";
+	document.getElementById("btn-register").style.display = "none";
 	console.log("boton registrar");
 }
 document.getElementById("btn-login").onclick = () => {
 	document.getElementById("register-container").style.display = "none";
 	document.getElementById("login-container").style.display = "flex";
+	document.getElementById("btn-register").style.display = "block";
+	document.getElementById("btn-login").style.display = "none";
 	console.log("boton login");
 }
